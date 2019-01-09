2cb02c204dd0eefd17130b03e6db596073ac1d59
Alexandr Timofeev <minimonium@outlook.com>
2019-01-09 03:06:44 +0300

    Fix inconsistent configs -> config

--- infra/services/dns/scripts/preconfigure.sh
+++ infra/services/dns/scripts/preconfigure.sh
@@ -4,5 +4,5 @@ echo "infra: DNS: Preconfiguration..."
 
 docker pull andyshinn/dnsmasq
 
-find ${INFRA_DIR}/configs -type f | xargs sed -i "s,\${infra.domain},${INFRA_DOMAIN},"
-find ${INFRA_DIR}/configs -type f | xargs sed -i "s,\${infra.ip},${INFRA_IP},"
+find ${INFRA_DIR}/config -type f | xargs sed -i "s,\${infra.domain},${INFRA_DOMAIN},"
+find ${INFRA_DIR}/config -type f | xargs sed -i "s,\${infra.ip},${INFRA_IP},"
