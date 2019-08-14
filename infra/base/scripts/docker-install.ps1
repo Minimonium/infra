717ecb7ca5f855d7028b51581a2d3e38eea3dfbc
Alexandr Timofeev <minimonium@outlook.com>
2019-08-14 15:33:04 +0300

    [base][windows] Install a fixed version of Docker due to an upstream bug

--- infra/base/scripts/docker-install.ps1
+++ infra/base/scripts/docker-install.ps1
@@ -1,6 +1,14 @@
 Write-Host "Installing Docker..."
 
+#Find-PackageProvider DockerMsftProvider | Install-PackageProvider -Force
 Install-PackageProvider -Name NuGet -Force
-Install-Module -Name DockerMsftProvider -Force
+#Install-Module -Name DockerMsftProvider -Force
+New-Item -ItemType Directory -Force -Path C:\Users\vagrant\AppData\Local\Temp\DockerMsftProvider\
+Write-Host "Start-BitsTransfer..."
+Start-BitsTransfer -Source https://dockermsft.blob.core.windows.net/dockercontainer/docker-19-03-1.zip -Destination C:\Users\vagrant\AppData\Local\Temp\DockerMsftProvider\Docker-19-03-1.zip
+Write-Host "Get-FileHash..."
+Get-FileHash -Path C:\Users\vagrant\AppData\Local\Temp\DockerMsftProvider\Docker-19-03-1.zip -Algorithm SHA256
+
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force -Verbose
-Start-Service docker 
+
+Start-Service docker
