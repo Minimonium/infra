93af07151500ed456f3cfcec9affe561e5241f68
Alexandr Timofeev <minimonium@outlook.com>
2019-08-19 14:11:34 +0300

    Basic dind configuration for Windows containers

--- infra/services/ci/scripts/build.ps1
+++ infra/services/ci/scripts/build.ps1
@@ -1,1 +1,3 @@
-docker build -t win1809-gitlab-runner -m 2GB ${workdir}/docker/images/win1809-gitlab-runner
+docker build -t gitlab-runner-base:1809 -m 2GB ${workdir}/docker/images/win/1809/gitlab-runner-base
+docker build -t gitlab-runner:1809 -m 2GB ${workdir}/docker/images/win/1809/gitlab-runner
+docker build -t gitlab-runner-dind:1809 -m 2GB ${workdir}/docker/images/win/1809/gitlab-runner-dind
--- infra/services/ci/scripts/run.sh
+++ infra/services/ci/scripts/run.sh
@@ -6,4 +6,5 @@ docker stack deploy \
 --compose-file ${INFRA_DIR}/docker/base.yml \
 --compose-file ${INFRA_DIR}/docker/gitlab-runner.lin.yml \
 --compose-file ${INFRA_DIR}/docker/gitlab-runner.win.yml \
+--compose-file ${INFRA_DIR}/docker/gitlab-runner-dind.win.yml \
 infra
