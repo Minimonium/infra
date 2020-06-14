64d774c7e4e78fa938e69f8ddd72617b394a39c5
Alexandr Timofeev <minimonium@outlook.com>
2020-06-14 22:17:47 +0300

    [gitlab] Skip artifacts in Gitlab backup

--- infra/services/core/backup.sh
+++ infra/services/core/backup.sh
@@ -13,7 +13,7 @@ sleep 2
 if docker inspect -f {{.State.Running}} gitlab_compose ; then
     echo "infra: Gitlab: Creating the Backup..."
     docker exec gitlab_compose bash -c 'gitlab-ctl reconfigure && \
-    gitlab-rake gitlab:backup:create && \
+    gitlab-backup create SKIP=artifacts && \
     cp /etc/gitlab/gitlab-secrets.json /media/backup'
 else
     echo "infra: Gitlab: CONTAINER IS NOT RUNNING"
