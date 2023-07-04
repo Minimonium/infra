d35752be8e9e79b527ddb8f0c829b2db31ea4d8d
Alexandr Timofeev <minimonium@outlook.com>
2023-07-04 18:37:23 +0200

    [taiga] Explicitly export taiga compose env

--- infra/services/core/scripts/run.sh
+++ infra/services/core/scripts/run.sh
@@ -2,6 +2,7 @@
 
 echo "infra: Running..."
 
+export ${INFRA_DIR}/config/taiga.env
 docker stack deploy \
 --compose-file ${INFRA_DIR}/docker/base.yml \
 --compose-file ${INFRA_DIR}/docker/visualizer.yml \
