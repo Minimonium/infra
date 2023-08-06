6061bef243c63823fa2da5e1144c4936c0a6d113
Alexandr Timofeev <minimonium@outlook.com>
2023-08-06 18:08:04 +0200

    Add provision to create default taiga/taiga superuser

--- infra/services/core/taiga-createuser.sh
+++ infra/services/core/taiga-createuser.sh
@@ -2,4 +2,4 @@
 
 export INFRA_DIR=/opt/infra/services/core
 
-source ${INFRA_DIR}/scripts/taiga-manage.sh createsuperuser
+source ${INFRA_DIR}/scripts/taiga-manage.sh createsuperuser --username=taiga --password=taiga --email=taiga@example.com
