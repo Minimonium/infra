c829e2edb235e8a27181ac993a0663d514fe0550
Alexandr Timofeev <minimonium@outlook.com>
2023-08-06 19:00:21 +0200

    Set up default password for Taiga superuser creation

--- infra/services/core/taiga-createuser.sh
+++ infra/services/core/taiga-createuser.sh
@@ -2,4 +2,4 @@
 
 export INFRA_DIR=/opt/infra/services/core
 
-source ${INFRA_DIR}/scripts/taiga-manage.sh createsuperuser --username=taiga --email=${INFRA_EMAIL} --noinput
+source ${INFRA_DIR}/scripts/taiga-manage.sh createsuperuser --username=infra --email=${INFRA_EMAIL} --noinput
