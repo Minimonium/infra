0440b0dde4cc7f83ec282e7c20e55b2f5b715472
Alexandr Timofeev <minimonium@outlook.com>
2023-08-31 22:43:23 +0200

    Fix issues with user

--- infra/services/core/taiga-restore.sh
+++ infra/services/core/taiga-restore.sh
@@ -11,18 +11,18 @@ docker service scale infra_taiga-events-rabbitmq=0 > /dev/null
 docker service scale infra_taiga-protected=0 > /dev/null
 docker service scale infra_taiga-gateway=0 > /dev/null
 
-docker service scale infra_taiga-back=0
+docker service scale infra_taiga-back=0 > /dev/null
 export TAIGA_DB_CONTAINER=$(docker container ps | grep infra_taiga-db | awk '{print $1}')
 docker exec ${TAIGA_DB_CONTAINER} bash -c 'BACKUP_FILE=$(find /media/backup/*_taiga-db-backup.sql) && \
-    chown taiga:taiga ${BACKUP_FILE} && \
+    chown $(id -u):$(id -g) ${BACKUP_FILE} && \
     psql -U taiga taiga < ${BACKUP_FILE}'
 
-docker service scale infra_taiga-back=1
+docker service scale infra_taiga-back=1 > /dev/null
 export TAIGA_BACK_CONTAINER=$(docker container ps | grep infra_taiga-back | awk '{print $1}')
 docker exec ${TAIGA_BACK_CONTAINER} bash -c 'BACKUP_FILE=$(find /media/backup/*_taiga-media-backup.tar.gz) && \
-    chown taiga:taiga ${BACKUP_FILE} && \
+    chown $(id -u):$(id -g) ${BACKUP_FILE} && \
     tar -xzvf ${BACKUP_FILE} -C /taiga-back/media --strip 1 && \
-    chown -R taiga:taiga /taiga-back/media'
+    chown -R $(id -u):$(id -g) /taiga-back/media'
 
 docker service scale infra_taiga-async=1 > /dev/null
 docker service scale infra_taiga-async-rabbitmq=1 > /dev/null
