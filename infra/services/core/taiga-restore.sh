eff095be9198fc8b71dd010bca6b883e3b3da3c2
Alexandr Timofeev <minimonium@outlook.com>
2023-08-31 22:16:26 +0200

    Fix typo

--- infra/services/core/taiga-restore.sh
+++ infra/services/core/taiga-restore.sh
@@ -11,16 +11,16 @@ docker service scale infra_taiga-events-rabbitmq=0
 docker service scale infra_taiga-protected=0
 docker service scale infra_taiga-gateway=0
 
-docker service scale taiga-back=0
+docker service scale infra_taiga-back=0
 export TAIGA_DB_CONTAINER=$(docker container ps | grep infra_taiga-db | awk '{print $1}')
-docker exec ${TAIGA_DB_CONTAINER} bash -c "BACKUP_FILE=$(find /media/backup/*_taiga_backup.sql) && \
-    psql -U taiga taiga < ${BACKUP_FILE}"
+docker exec ${TAIGA_DB_CONTAINER} bash -c 'BACKUP_FILE=$(find /media/backup/*_taiga_backup.sql) && \
+    psql -U taiga taiga < ${BACKUP_FILE}'
 
-docker service scale taiga-back=1
+docker service scale infra_taiga-back=1
 export TAIGA_BACK_CONTAINER=$(docker container ps | grep infra_taiga-back | awk '{print $1}')
-docker exec ${TAIGA_BACK_CONTAINER} bash -c "BACKUP_FILE=$(find /media/backup/*_taiga_backup.tar.gz) && \
+docker exec ${TAIGA_BACK_CONTAINER} bash -c 'BACKUP_FILE=$(find /media/backup/*_taiga_backup.tar.gz) && \
     tar -xzvf ${BACKUP_FILE} -C /taiga-back/media --strip 1 && \
-    chown -R taiga:taiga /taiga-back/media"
+    chown -R taiga:taiga /taiga-back/media'
 
 docker service scale infra_taiga-async=1
 docker service scale infra_taiga-async-rabbitmq=1
